3a263edd20147b843d6566abcd77fc06207e95aa
Rocky Dunlap <dunlap@ucar.edu>
2022-07-15 12:45:50 -0600

    action=collect dir=gfortran_10.3.0_openmpi_O_develop branch=develop hash=v8.4.0b04-2-gc831a7c

---
+++ develop/gfortran/10.3.0/O/openmpi/openmpi-4.1.0/out/build.bat
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+export JOBID=NO_BATCH
+module load gfortran-10.3.0 openmpi-4.1.0
+module load netcdf-4.7.4
+
+set -x
+export PATH=/home/rocky/esmfdev/cmake-3.22.1-install/bin:$PATH
+export ESMF_DIR=/home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/module-build.log
+cd /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
+make info 2>&1| tee ../info.log
+echo `date` > ../build.log
+echo 'FAKE BUILD JOB COMPLETE' >> ../build.log
