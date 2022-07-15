745e93ba23dbe4ba746c86b8f7d6c17c6fe83d30
Rocky Dunlap <dunlap@ucar.edu>
2022-07-15 13:04:03 -0600

    action=clear dir=gfortran_10.3.0_openmpi_g_develop branch=develop hash=v8.4.0b04-2-gc831a7c

--- develop/gfortran/10.3.0/O/openmpi/openmpi-4.1.0/out/test.bat
+++ develop/gfortran/10.3.0/O/openmpi/openmpi-4.1.0/out/test.bat
@@ -5,13 +5,13 @@ module load netcdf-4.7.4
 
 set -x
 export PATH=/home/rocky/esmfdev/cmake-3.22.1-install/bin:$PATH
-export ESMF_DIR=/home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/esmf
+export ESMF_DIR=/home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_NETCDF=nc-config
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-module list >& /home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/module-test.log
-cd /home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/esmf
+module list >& /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/module-test.log
+cd /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
 echo 'FAKE TEST JOB COMPLETE' >> ../test.log
