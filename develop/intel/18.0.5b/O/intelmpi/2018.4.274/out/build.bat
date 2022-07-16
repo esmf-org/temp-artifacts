e19f1eedc8e04900159bf015e66f53966feffee0
Rocky Dunlap <Rocky.Dunlap@noaa.gov>
2022-07-16 19:04:00 +0000

    action=collect dir=intel_18.0.5b_intelmpi_O_develop branch=develop hash=v8.4.0b04-2-gc831a7c

---
+++ develop/intel/18.0.5b/O/intelmpi/2018.4.274/out/build.bat
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_develop/build.bat_%j.o
+#SBATCH -e /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load intel/18.0.5.274 impi/2018.4.274
+module load netcdf-hdf5parallel/4.7.4
+
+set -x
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_develop/module-build.log
+cd /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_develop/esmf
+make info 2>&1| tee ../info.log
+make -j 40 2>&1| tee ../build.log
