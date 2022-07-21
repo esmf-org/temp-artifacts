37dcdb373b941f7800b19ccbf3dd9a7566fa2afa
Rocky Dunlap <Rocky.Dunlap@noaa.gov>
2022-07-21 20:15:37 +0000

    action=collect dir=intel_18.0.5b_intelmpi_O_dushan_dev_info_fix branch=dushan_dev_info_fix hash=v8.4.0b06-9-g3217834

---
+++ dushan_dev_info_fix/intel/18.0.5b/O/intelmpi/2018.4.274/out/test.bat
@@ -0,0 +1,32 @@
+#!/bin/sh -l
+#SBATCH --account=nems
+#SBATCH -o /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_dushan_dev_info_fix/test.bat_%j.o
+#SBATCH -e /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_dushan_dev_info_fix/test.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=hera
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load intel/18.0.5.274 impi/2018.4.274
+module load netcdf-hdf5parallel/4.7.4
+
+set -x
+tar xvfz ~/pytest-input.tar.gz
+export ESMF_DIR=/scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_dushan_dev_info_fix/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_dushan_dev_info_fix/module-test.log
+cd /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-test-dev/intel_18.0.5b_intelmpi_O_dushan_dev_info_fix/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
