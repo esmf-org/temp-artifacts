6ecf7556d9b0ae061d6ce125be4c72b1f8a4993d
Rocky Dunlap <dunlap@ucar.edu>
2022-07-20 23:07:41 -0600

    action=collect dir=gfortran_10.3.0_openmpi_O_develop branch=develop hash=v8.4.0b06-6-g1266599

--- develop/gfortran/10.3.0/O/openmpi/openmpi-4.1.0/out/build.bat
+++ develop/gfortran/10.3.0/O/openmpi/openmpi-4.1.0/out/build.bat
@@ -5,15 +5,15 @@ module load netcdf-4.7.4
 
 set -x
 export PATH=/home/rocky/esmfdev/cmake-3.22.1-install/bin:$PATH
-export ESMF_DIR=/home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/esmf
+export ESMF_DIR=/home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_NETCDF=nc-config
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-module list >& /home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/module-build.log
-cd /home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/esmf
+module list >& /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/module-build.log
+cd /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
+set -o pipefail
 make info 2>&1| tee ../info.log
-echo `date` > ../build.log
-echo 'FAKE BUILD JOB COMPLETE' >> ../build.log
+make -j 4 2>&1| tee ../build.log
--- develop/gfortran/10.3.0/O/openmpi/openmpi-4.1.0/out/test.bat
+++ develop/gfortran/10.3.0/O/openmpi/openmpi-4.1.0/out/test.bat
@@ -5,13 +5,17 @@ module load netcdf-4.7.4
 
 set -x
 export PATH=/home/rocky/esmfdev/cmake-3.22.1-install/bin:$PATH
-export ESMF_DIR=/home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/esmf
+export ESMF_DIR=/home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=openmpi
 export ESMF_NETCDF=nc-config
 export ESMF_BOPT='O'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-module list >& /home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/module-test.log
-cd /home/rocky/esmfdev/esmf-testing-scratch2/gfortran_10.3.0_openmpi_O_develop/esmf
-echo 'FAKE TEST JOB COMPLETE' >> ../test.log
+module list >& /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/module-test.log
+cd /home/rocky/esmfdev/esmf-testing-scratch/gfortran_10.3.0_openmpi_O_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
